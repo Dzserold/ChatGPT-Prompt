ca7dbdb01a9d5d256a182dafb67429e2e521497d
Dzserold <f.dzserold@gmail.com>
2024-06-13 01:19:31 +0200

    feedback to user on processing data

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -7,15 +7,18 @@ import { PromptCard } from "./PromptCard";
 export default function Input() {
   const [input, setInput] = useState("");
   const [prompts, setPrompts] = useState<any[]>([]);
+  const [isPendling, setIsPendling] = useState(false);
 
   useEffect(() => {
     getFromAI();
   }, []);
 
   const postToAI = async () => {
+    setIsPendling(true);
     const response = await postPrompt(input);
-    if (response.staus === 200) getFromAI();
+    if (response) getFromAI();
     setInput("");
+    setIsPendling(false);
   };
 
   const getFromAI = async () => {
@@ -25,7 +28,7 @@ export default function Input() {
   };
 
   return (
-    <div className="flex gap-3 flex-col items-center min-h-screen p-4 text-center">
+    <div className="flex flex-col items-center min-h-screen gap-3 p-4 text-center">
       <form
         className="flex flex-col items-center gap-3"
         action=""
@@ -36,16 +39,21 @@ export default function Input() {
         </p>
         <textarea
           className="w-full p-1 font-semibold text-green-700 border-4 rounded-md outline-none bg-slate-200 focus:border-green-800"
+          disabled={isPendling}
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
         <button
           className="w-40 p-1 text-lg font-bold text-black bg-green-600 rounded-md hover:scale-105"
           type="button"
+          disabled={isPendling}
           onClick={() => postToAI()}
         >
           Enter
         </button>
+        <p className="h-6 text-lg text-yellow-300">
+          {isPendling && "The AI is thinking...."}
+        </p>
       </form>
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
         {prompts.length > 0 &&
